b3a178de547d455aa34fabc2a8a25ca988e626ac
Treyvon <trey2044@outlook.com>
2026-02-01 20:44:29 -0500

    third

--- js02_txt.js
+++ js02_txt.js
@@ -4,9 +4,51 @@
 
       Fan Trick Fine Art Photography
       Variables and functions
-      Author: 
-      Date:   
+      Author: Treyvon Pearson
+      Date: 2/1/26
 
       Filename: js02.js
  */
+// set the form's default values
+// declare global constatns for the application
+const EMP_COST = 100; //cost of photographers per hour
+const BOOK_COST =350; //cost of memory book
+const REPRO_COST = 1250; //cost of reproduction rights
+const TRAVEL_COST = 2; //cost of travel per mile
 
+window.addEventListener("load", setupForm)
+function setupForm() {
+      document.getElementById("photoNum").value = 1;
+      document.getElementById("photoHrs").value = 2;
+      document.getElementById("makeBook").checked = false;
+      document.getElementById("photoRights").checked = false;
+      document.getElementById("photoDist").value = 0;
+  
+      getEstimate();
+  
+      document.getElementById("photoNum").onchange = getEstimate;
+      document.getElementById("photoHrs").onchange = getEstimate;
+      document.getElementById("photoDist").onchange = getEstimate;
+      document.getElementById("makeBook").onchange = getEstimate;
+      document.getElementById("photoRights").onchange = getEstimate;
+  }
+  
+function getEstimate() {
+      // Add the cost of photographers for the hours covered
+      let totalCost = 0;
+      let photographers = document.getElementById("photoNum").value;
+      let hours = document.getElementById("photoHrs").value;
+      let distance = document.getElementById("photoDist").value;
+      let buyBook = document.getElementById("makeBook").checked;
+      let buyRights = document.getElementById("photoRights").checked;
+      // Add the cost of photographers for the hours covered
+      totalCost += photographers * hours * EMP_COST;
+      // add the cost of distance per photographer per tile
+      totalCost += photographers * distance * TRAVEL_COST;
+      //Add the cost of the book if purchased
+      totalCost += buyBook ? REPRO_COST: 0;
+      //Add the cost of photo rights if purchased
+      totalCost += buyRights ? REPRO_COST: 0;
+      // Display the total cost estimate
+      document.getElementById("estimate").innerHTML = "$" + totalCost;
+}
